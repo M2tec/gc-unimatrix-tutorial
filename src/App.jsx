6752581fc04c4b3f39b35b3c9b15c4e49cf38ab2
Maarten Menheere <m.menheere@m2tec.nl>
2025-01-15 10:37:04 +0100

    Edit and save user data

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ function App() {
   const [members, setMembers] = useState({})
   const [proposals, setProposals] = useState({})
 
+  const controllerMember = { 0: { type: "Controller", name: "John", member_address: "" } }
+
+
+
   useEffect(() => {
 
     // Setup Unimatrix
@@ -57,8 +61,7 @@ function App() {
 
     console.log(getMembers)
     if (getMembers === null) {
-      console.log("null null null")
-      getMembers = { 0: { type: "Controller", name: "maarten", member_address: "xxxx" } }
+      getMembers = controllerMember
     }
 
     setMembers({ ...members, ...getMembers })
@@ -66,12 +69,38 @@ function App() {
     let getProposals = JSON.parse(localStorage.getItem("proposals_0"));
 
     if (getProposals === null) {
-      console.log("null null null")
       getProposals = { 0: { type: "Governance", name: "New prop", details: "Payout contractor 5 ADA", receiver_address: "xxxx", amount: 5 } }
     }
 
     setProposals({ ...proposals, ...getProposals })
   }, []);
+
+  
+  async function handleNameChange(event, index) {
+
+      console.log("Change name");
+
+      let changedMembers = {...members}
+      changedMembers[index].name = event.target.value
+      setMembers({ ...changedMembers })
+
+      console.log(members)
+      localStorage.setItem("members_0", JSON.stringify({ ...members }))
+
+  }
+
+  async function handleAddressChange(event, index) {
+
+    console.log("Change address");
+
+    let changedMembers = {...members}
+    changedMembers[index].member_address = event.target.value
+    setMembers({ ...changedMembers })
+
+    console.log(members)
+    localStorage.setItem("members_0", JSON.stringify({ ...members }))
+
+}
 
   async function handleAddMember(e) {
 
@@ -91,6 +120,14 @@ function App() {
     console.log(members)
 
   }
+
+  async function resetMembers(e) {
+    localStorage.setItem("members_0", JSON.stringify(controllerMember))
+    let getMembers = JSON.parse(localStorage.getItem("members_0"));
+    setMembers({...getMembers })
+    console.log("Reset")
+  } 
+
 
   async function handleCreateDAO(e) {
 
@@ -130,14 +167,14 @@ function App() {
         <div className="input-group-prepend">
           <span className="input-group-text" id="basic-addon1">@</span>
         </div>
-        <input type="text" className="form-control" placeholder="Username" defaultValue={members[item].name} aria-label="Username" aria-describedby="basic-addon1" />
+        <input type="text" onChange={(e) => handleNameChange(e,index)} className="form-control" placeholder="Username" defaultValue={members[item].name} aria-label="Username" aria-describedby="basic-addon1" />
       </div>
 
       <div className="input-group mb-3">
         <div className="input-group-prepend">
           <span className="input-group-text" id="basic-addon1">Address</span>
         </div>
-        <input type="text" className="form-control" placeholder="Cardano address" defaultValue={members[item].member_address} aria-label="Username" aria-describedby="basic-addon1" />
+        <input type="text" onChange={(e) => handleAddressChange(e,index)} className="form-control" placeholder="Cardano address" defaultValue={members[item].member_address} aria-label="Username" aria-describedby="basic-addon1" />
       </div>
     </li>
 
@@ -189,6 +226,7 @@ function App() {
           <ul className="list-group">
             {listMembers}
             <a className="btn btn-primary mt-2" onClick={handleAddMember}>Add member</a>
+            <a className="btn btn-primary mt-2" onClick={resetMembers}>Reset</a>
           </ul>
           <a className="btn btn-primary m-3" onClick={handleCreateDAO}>Create DAO</a>
         </div>
